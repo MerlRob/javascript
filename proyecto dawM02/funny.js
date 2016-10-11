a9091b420c90d082c3a57e3ae468191899d368d8
MerlRob <merlrobsal@gmail.com>
2016-10-11 13:47:58 +0200

    más ejercicio 7

---
+++ proyecto dawM02/funny.js
@@ -0,0 +1,67 @@
+
+
+/*var cuenta = new Array(4);
+var totalAcreedor= 0;
+var suma= 0;
+
+	for(var i= 0; i < cuenta.length; i++)
+	{
+		cuenta[0] = prompt("Introduce el numero de cuenta");
+	
+		cuenta[1] = prompt("Introduce el nombre del cliente");
+		
+		cuenta[2] = prompt("Introduce el importe");
+		
+			if(cuenta[0] < 0){
+				break;	
+			
+				cuenta.pop(cuenta.length -1);
+			}
+				else if(cuenta[2] > 0){
+					cuenta[3] = "Acreedor"; 
+					
+						suma = cuenta[2];
+					
+							}else if(cuenta[2] < 0){
+								cuenta[3] = "Deudor";
+								}else{
+									cuenta[3] = "Nulo";			
+				} 
+		
+		document.write("Numero de cuenta es: " + cuenta[0] + "&emsp;" +" Nombre de cliente: " + cuenta[1] + "&emsp;" +" Saldo: " +cuenta[2] + "&emsp;" +" Estado: " + cuenta[3] + "<br>");
+		cuenta.length++;
+		
+		totalAcreedor=totalAcreedor + parseInt(suma);
+		}
+		document.write("Total del saldo acreedor: " + totalAcreedor);	*/
+		
+		
+		/*
+		
+		var base = 0;
+		var altura = 0;
+		var superficie = 0;
+		var count= 0;
+		
+		for(var i= 0; i < 3; i++){
+			base = prompt("Escribre la medida de la base ");
+			
+			altura = prompt("Escribe la medida de la altura");
+			superficie=(parseInt(base)*parseInt(altura))/2;
+			
+			
+			document.write("la base es " + base + "<br>" + ", la altura es " + altura + "<br>" +" y la superficie es" + superficie + "<br><br>");
+			
+			if(superficie> 12)
+			{
+				count++;
+			}
+		}
+		document.write("Hay " + count + " triangulos cuya superficie es mayor a 12");
+	
+	*/
+	
+	
+	
+		
+		
